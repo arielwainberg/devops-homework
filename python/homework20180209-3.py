b776823390791b02b108eafdaef17cb903fa77b1
Ariel Wainberg <arielw81@gmail.com>
2018-02-09 19:13:36 +0200

    3th Mission Completed

---
+++ python/homework20180209-3.py
@@ -0,0 +1,11 @@
+#3- Write a Python program that receives the Name of the user and his age,
+#then prints the Name and on which year he/she will reach the age of 120!
+#To get user input use the input function
+#name = input('Please enter your name:')
+import datetime
+now = datetime.datetime.now()
+currentyear = now.year
+dstage = 120
+name = input("What is your name?")
+age = input("How old are you?")
+print(f"{name}, at year {(currentyear) + int(age)}, you will be {dstage} years old")
